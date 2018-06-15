98afaa3551577279d0286c7c3a24762a81f7eec9
Martin Dvorak <martin.dvorak@mindforger.com>
2018-06-15 10:37:16 +0200

    Switching unit script to seek for memleaks.

--- build/test-lib-units.sh
+++ build/test-lib-units.sh
@@ -19,7 +19,7 @@
 
 export OPTION_RUN_ALL_TESTS=yes # comment this line to run OPTION_TEST only
 export OPTION_RECOMPILE=yes # recompile before running test(s) (comment this line to disable)
-#export OPTION_RUN_VALGRIND=yes # run test(s) w/ Valgrind (comment this line to disable)
+export OPTION_RUN_VALGRIND=yes # run test(s) w/ Valgrind (comment this line to disable)
 #export OPTION_RUN_GDB=yes # run test(s) w/ GDB (comment this line to disable)
 
 # benchmarks
