6694a331f7c220c32970225eba9e211976b46d5a
Martin Dvorak <martin.dvorak@mindforger.com>
2018-10-05 11:05:09 +0200

    Making CSS more compact for #831.

--- lib/src/config/configuration.h
+++ lib/src/config/configuration.h
@@ -66,6 +66,7 @@ constexpr const auto UI_THEME_LIGHT = "light";
 constexpr const auto UI_THEME_BLACK = "black";
 
 constexpr const auto UI_HTML_THEME_CSS_LIGHT = "qrc:/html-css/light.css";
+constexpr const auto UI_HTML_THEME_CSS_LIGHT_COMPACT = "qrc:/html-css/light-compact.css";
 constexpr const auto UI_HTML_THEME_CSS_DARK = "qrc:/html-css/dark.css";
 constexpr const auto UI_HTML_THEME_CSS_RAW = "raw";
 
