e9e714124e563b768d55585bad571230360cec3f
Martin Dvorak <martin.dvorak@mindforger.com>
2018-12-26 08:59:22 +0100

    Adding autolinking a-c description.

--- lib/src/mind/ai/autolinking/aho_corasick_autolinking_preprocessor.h
+++ lib/src/mind/ai/autolinking/aho_corasick_autolinking_preprocessor.h
@@ -25,6 +25,56 @@
 
 namespace m8r {
 
+/**
+ * @brief Aho-Corasick search based autolinking pre-processor.
+ *
+ * This autolinking implementation has two goals:
+ *
+ *  - performance
+ *  - correctness
+ *
+ * It aims to be FAST, therefore it's based on Aho-Corasick search
+ * structure when autolinking lines.
+ *
+ * It aims to AVOID BREAKING rendered HTML by partial parsing of MD
+ * constructions and autolinking only safe segments of line's AST.
+ *
+ * Algorithm description:
+ *
+ *  - O or N description is autolinked
+ *  - O or N or T links are injected
+ *
+ * Preparation:
+ *
+ *  1. Aho-Corasick search structure is (re)build:
+ *    - Aho-Corasick structure is Trie w/ internal links
+ *    - the structure can be CACHED and rebuilt only when name or description
+ *      of O/N is changed OR tag is added/removed
+ *
+ * Method:
+ *
+ *  1. Description is processed LINE by LINE
+ *    a) if LINE is in code or math BLOCK, then it's skipped
+ *  2. LINE is partially parsed for INLINED code, math, link, ... constructions
+ *     to get line AST with TEXT SECTIONS which can be safely autolinked
+ *  3. Text sections are searched for O/N/T names presense using Aho-Corasick
+ *     search structure:
+ *    a) Aho-Corasics structure searches for all O/N/T names at once in single pass
+ *    b) Aho-Corasics structure has LINEAR COMPLEXITY
+ *    c) Aho-Corasics structure gives ALL MATCHES found in the text
+ *    d) every MATCH to have:
+ *      - begin offset
+ *      - end offset
+ *      - length
+ *      - type O/N/T
+ *    e) in case of overlapping/included MATCHES:
+ *      - longest match always wins
+ *  4. When there is O/N/T name clash (e.g. `ML` can be Outline, Note and Tag, actually
+ *     there can be even more Notes with this name), then these matches are remembered
+ *     and when user CLICKs the link in the description, then DIALOG with all options
+ *     is opened (think Wikipedia cross-road page) and user can choose which link
+ *     to use.
+ */
 class AhoCorasickAutolinkingPreprocessor : public AutolinkingPreprocessor
 {
     Mind& mind;
