4df5002d963e6e206856e8f24a70dbd8aee1e8cc
Martin Dvorak <martin.dvorak@mindforger.com>
2019-01-14 23:04:03 +0100

    Updating source codes 2018 > 2019

--- lib/src/representations/markdown/markdown_outline_representation.h
+++ lib/src/representations/markdown/markdown_outline_representation.h
@@ -1,7 +1,7 @@
 /*
  markdown_outline_representation.h     MindForger thinking notebook
 
- Copyright (C) 2016-2018 Martin Dvorak <martin.dvorak@mindforger.com>
+ Copyright (C) 2016-2019 Martin Dvorak <martin.dvorak@mindforger.com>
 
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
