e573715069917014636f7d64dcf771827c8a7241
Vlasta Hajek <vlastimil.hajek@gmail.com>
2019-01-25 17:48:01 +0100

    Proper export - #648

--- build/windows/strptime/strptime.h
+++ build/windows/strptime/strptime.h
@@ -2,6 +2,14 @@
 #define STRPTIME_H
 
 #include <time.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 char   *strptime(const char *, const char *, struct tm *);
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif //STRPTIME_H
