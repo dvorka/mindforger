c03dd5f02125115c1ea7660b42b8d9645a590af6
Martin Dvorak <martin.dvorak@mindforger.com>
2020-01-11 11:04:29 +0100

    Adding name to saved N message.

--- app/src/qt/note_edit_presenter.cpp
+++ app/src/qt/note_edit_presenter.cpp
@@ -113,8 +113,7 @@ void NoteEditPresenter::slotSaveNote()
 
         // remember
         mwp->getMind()->remind().remember(currentNote->getOutlineKey());
-        mwp->getStatusBar()->showInfo(tr("Note saved!"));
-        MF_DEBUG("Note '" << currentNote->getName() << "' saved!" << endl);
+        mwp->getStatusBar()->showInfo(tr("Note '%1' saved").arg(QString::fromStdString(currentNote->getName())));
     } else {
         mwp->getStatusBar()->showError(tr("Attempt to save data from UI to Note, but no Note is set."));
     }
