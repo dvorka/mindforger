8f6123a0530b8d6d8b3a95336312a644221f9b9f
Martin Dvorak <martin.dvorak@mindforger.com>
2020-03-08 12:38:59 +0100

    Fixing clean Ubuntu build script.

--- build/ubuntu/build-all-clean-system.sh
+++ build/ubuntu/build-all-clean-system.sh
@@ -32,7 +32,7 @@ git submodule update
 sudo apt-get install build-essential zlib1g-dev libqt5webkit5-dev qttools5-dev-tools qt5-default ccache
 
 # deps build
-cd  ${SCRIPT_HOME}/deps/cmark-gfm
+cd  ${SCRIPT_HOME}/../../deps/cmark-gfm
 mkdir build && cd build
 cmake -DCMARK_TESTS=OFF -DCMARK_SHARED=OFF ..
 cmake --build .
