a8b04f6485344d572a2f981fbe67249f2d5f23d0
Martin Dvorak <martin.dvorak@mindforger.com>
2021-12-17 10:50:05 +0100

    Updating 2021 to 2022 in copyright headers, build scripts and template generators.

--- app/src/qt/dialogs/export_csv_file_dialog.cpp
+++ app/src/qt/dialogs/export_csv_file_dialog.cpp
@@ -1,7 +1,7 @@
 /*
  export_csv_file_dialog.cpp     MindForger thinking notebook
 
- Copyright (C) 2016-2021 Martin Dvorak <martin.dvorak@mindforger.com>
+ Copyright (C) 2016-2022 Martin Dvorak <martin.dvorak@mindforger.com>
 
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
