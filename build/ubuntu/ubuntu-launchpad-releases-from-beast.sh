21ee305a22a29216123848b1a2d7e99e6d1bdc1e
Martin Dvorak <martin.dvorak@mindforger.com>
2021-12-26 15:16:12 +0100

    Ubuntu 1.53.0 Launchpad release of 5 current distros (except Trusty) done.

--- build/ubuntu/ubuntu-launchpad-releases-from-beast.sh
+++ build/ubuntu/ubuntu-launchpad-releases-from-beast.sh
@@ -212,7 +212,7 @@ export ARG_BAZAAR_MSG="MindForger ${ARG_MAJOR_VERSION}${ARG_MINOR_VERSION} relea
 # https://wiki.ubuntu.com/Releases
 # obsolete: precise quantal saucy utopic vivid wily yakkety artful cosmic disco eoan groovy
 # current : trusty xenial bionic focal hirsute impish
-# 1.53.x  : trusty xenial bionic | focal hirsute impish
+# 1.53.x  : (trusty: compilation fail) xenial bionic focal hirsute impish
 for UBUNTU_VERSION in focal hirsute impish
 do
     echo "Releasing MF for Ubuntu version: ${UBUNTU_VERSION}"
