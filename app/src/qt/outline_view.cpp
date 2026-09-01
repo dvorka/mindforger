dce740b069150418258aa70e7be5b048bdf3e337
Martin Dvorak <martin.dvorak@mindforger.com>
2026-09-01 15:43:39 +0200

    fix(O): 🐞 fixing rendering of the O name even after MF starts

--- app/src/qt/outline_view.cpp
+++ app/src/qt/outline_view.cpp
@@ -58,12 +58,17 @@ OutlineView::~OutlineView()
 
 void OutlineView::refreshHeader(const std::string& name)
 {
-    if(!name.empty()) {
+    currentName = name;
+    updateNameLabelText();
+}
+
+void OutlineView::updateNameLabelText()
+{
+    if(!currentName.empty()) {
         QFontMetrics metrics(nameLabel->font());
-        // IMPROVE nameLabel has incorrect size before rendered for the first time - find a better solution than fixed width
         QString elidedText
             = metrics.elidedText(
-               QString::fromStdString(name),
+               QString::fromStdString(currentName),
                Qt::ElideRight,
                width()-20);
         // escape '&' in the outline name to avoid Qt interpreting it as a shortcut
@@ -72,6 +77,12 @@ void OutlineView::refreshHeader(const std::string& name)
     } else {
         nameLabel->setText("");
     }
+}
+
+void OutlineView::resizeEvent(QResizeEvent* event)
+{
+    QWidget::resizeEvent(event);
+    updateNameLabelText();
 }
 
 } // m8r namespace
