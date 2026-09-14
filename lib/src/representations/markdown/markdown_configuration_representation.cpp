d9191f15d0dfbc8eef32be44315e5cd326016dba
Martin Dvorak <martin.dvorak@mindforger.com>
2026-09-14 21:45:46 +0200

    test(src highlight): 🧪 improving test coverage of the source code syntax highlighting

--- lib/src/representations/markdown/markdown_configuration_representation.cpp
+++ lib/src/representations/markdown/markdown_configuration_representation.cpp
@@ -742,7 +742,7 @@ string& MarkdownConfigurationRepresentation::to(Configuration* c, string& md)
          "    * Enable offline KaTeX or MathJax (legacy) JavaScript library to show math expressions in HTML generated from Markdown." << endl <<
          "    * Examples: katex, mathjax, no" << endl <<
          CONFIG_SETTING_MD_DIAGRAM_LABEL << (c?c->getJsLibSupportAsString(c->getUiEnableDiagramsInMd()):UI_JS_LIB_NO) << endl <<
-         "    * Enable online or offline Mermaid JavaScript library to show diagrams in HTML generated from Markdown." << endl <<
+         "    * Enable offline Mermaid JavaScript library to show diagrams in HTML generated from Markdown." << endl <<
          "    * Examples: offline, no" << endl <<
          CONFIG_SETTING_NAVIGATOR_MAX_GRAPH_NODES_LABEL << (c?c->getNavigatorMaxNodes():Configuration::DEFAULT_NAVIGATOR_MAX_GRAPH_NODES) << endl <<
          "    * Maximum number of knowledge graph navigator nodes (performance vs. readability trade-off)." << endl <<
